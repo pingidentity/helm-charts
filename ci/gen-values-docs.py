e387752e47e962e39e52ab251338af793c5ef2cb
Henry Recker <henryrecker@pingidentity.com>
2021-12-27 17:12:12 -0600

    Add support for defining sections

--- ci/gen-values-docs.py
+++ ci/gen-values-docs.py
@@ -7,7 +7,9 @@ import sys
 def fail(message):
     exit("❌ Error: {}".format(message))
 
-def generateMarkdown(params):
+markdownHeader="## Supported values"
+
+def generateTable(params):
     if len(params) == 0:
         return ""
 
@@ -30,7 +32,6 @@ def generateMarkdown(params):
     markdown = "| {col1: <{col1Len}} | {col2: <{col2Len}} | {col3: <{col3Len}} |\n".format(
         col1="Name", col1Len=maxLenName, col2="Description", col2Len=maxLenDesc, col3="Default", col3Len=maxLenDefault)
     markdown += "| " + ("-" * maxLenName) + " | " + ("-" * maxLenDesc) + " | " + ("-" * maxLenDefault) + " |\n"
-    #TODO handle empty values like empty string in the table?
     for param in params:
         nameCol = "`" + param['name'] + "`"
         defaultCol = ""
@@ -42,13 +43,33 @@ def generateMarkdown(params):
 
     return markdown
 
+def generateMarkdown(sections):
+    markdown = markdownHeader + "\n"
+    if len(sections) == 0:
+        return markdown
+
+    for sectionName in sections.keys():
+        if sections[sectionName]:
+            if sectionName != "undefined":
+                markdown += "### " + sectionName + "\n"
+            markdown += generateTable(sections[sectionName])
+    
+    return markdown
+
 # Add some defaults for empty params
-def addParam(list, newParam):
+def addParam(sections, newParam):
     if "description" not in newParam:
         newParam["description"] = ""
     if "default" not in newParam:
         newParam["default"] = ""
-    list.append(newParam)
+
+    section = "undefined"
+    if "section" in newParam:
+        section = newParam["section"]
+    
+    if section not in sections:
+        sections[section] = []
+    sections[section].append(newParam)
 
 if len(sys.argv) not in [2, 3]:
     fail("Expected one or two arguments. Name of file to generate docs for, and optional file to write result to. Got {} args.".format(len(sys.argv) - 1))
@@ -57,32 +78,37 @@ srcFilename = sys.argv[1]
 destFilename = ""
 if len(sys.argv) == 3:
     destFilename = sys.argv[2]
-params = []
+sections = {}
 currentParam = {}
+currentSection = ""
 try:
     with open(srcFilename) as input:
         print("Generating documentation for file {}".format(srcFilename))
         for line in input:
             split = line.strip().split()
             if len(split) >= 3:
+                if split[1] == "@section":
+                    currentSection = " ".join(split[2:])
                 if split[1] == "@param":
                     if currentParam:
-                        addParam(params, currentParam)
+                        addParam(sections, currentParam)
                         currentParam = {}
+                        if currentSection:
+                            currentParam["section"] = currentSection
                     currentParam["name"] = split[2]
                     if len(split) >= 4:
                         currentParam["description"] = " ".join(split[3:])
                 elif split[1] == "@default":
                     currentParam["default"] = " ".join(split[2:])
         if currentParam:
-            addParam(params, currentParam)
+            addParam(sections, currentParam)
 
 except FileNotFoundError:
     fail("Specified source file {} does not exist.".format(srcFilename))
 except IsADirectoryError:
     fail("Specified source file {} is a directory. Please specify a valid values file.".format(srcFilename))
 
-markdown = generateMarkdown(params)
+markdown = generateMarkdown(sections)
 if destFilename:
     with open(destFilename, "w") as dest:
         print("Writing result to file {}".format(destFilename))
