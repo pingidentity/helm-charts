82b4a1eb2669014e454a59f9d98199696739f6f7
wesleymccollam <wesleymccollam@pingidentity.com>
2022-01-05 13:48:03 -0600

    Added jq to initialization stage

--- ci_scripts/shellcheck.sh
+++ ci_scripts/shellcheck.sh
@@ -7,6 +7,11 @@
 #
 test "${VERBOSE}" = "true" && set -x
 
+#gimme jq
+JQ=/usr/bin/jq
+curl -sL https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 > $JQ && chmod +x $JQ
+ls -la $JQ
+
 command_prefix=""
 if ! type shellcheck; then
     if test "$(uname -m)" = "x86_64" && test "$(uname -s)" = "Linux"; then
--- ci_scripts/shfmt.sh
+++ ci_scripts/shfmt.sh
@@ -9,6 +9,11 @@
 #
 test "${VERBOSE}" = "true" && set -x
 
+#gimme jq
+JQ=/usr/bin/jq
+curl -sL https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 > $JQ && chmod +x $JQ
+ls -la $JQ
+
 # Usage printing function
 usage() {
     test -n "${*}" && echo "${*}"
