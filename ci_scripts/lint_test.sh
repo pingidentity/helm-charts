a86c3bb3f6ef9966f3f6246fed51eff585fd0e11
wesleymccollam <wesleymccollam@pingidentity.com>
2022-01-18 14:22:53 -0600

    Quoting release branch

--- ci_scripts/lint_test.sh
+++ ci_scripts/lint_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone -b $CI_COMMIT_BRANCH "https://${GITLAB_USER}:${GITLAB_TOKEN}@${INTERNAL_GITLAB_URL}/devops-program/helm-charts"
+git clone -b "$CI_COMMIT_BRANCH" "https://${GITLAB_USER}:${GITLAB_TOKEN}@${INTERNAL_GITLAB_URL}/devops-program/helm-charts"
 cd helm-charts || exit
 git config user.email "devops_program@pingidentity.com"
 git config user.name "devops_program"
