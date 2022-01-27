e69f3d100116629a9750514c7ce764c9b431c50f
wesleymccollam <wesleymccollam@pingidentity.com>
2022-01-27 14:08:17 -0600

    0.8.5

--- ci_scripts/release_charts.sh
+++ ci_scripts/release_charts.sh
@@ -46,12 +46,12 @@ function update_chart_index() {
 function publish_charts() {
     #change this to the real repo
     git clone "https://${GITHUB_OWNER}:${GITHUB_TOKEN}@github.com/wesleymccollam/helm-charts-test.git"
-    git config user.email "${GITHUB_OWNER}@pingidentity.com"
-    git config user.name "${GITHUB_OWNER}"
     yes | cp ${pwd}/docs/index.yaml helm-charts-test/docs/index.yaml
     cd helm-charts-test
     git add docs/index.yaml
     git commit -m="Release $CI_COMMIT_BRANCH"
+    git config user.email "${GITHUB_OWNER}@pingidentity.com"
+    git config user.name "${GITHUB_OWNER}"
     git remote add gh_location "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/wesleymccollam/helm-charts-test.git"
     if test -n "$CI_COMMIT_TAG"; then
         git push gh_location "$CI_COMMIT_TAG"
