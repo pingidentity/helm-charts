b5baaa05057001e28796510d66e43b48b298fdd4
wesleymccollam <wesleymccollam@pingidentity.com>
2022-01-27 12:08:25 -0600

    get ci commit message

--- ci_scripts/release_charts.sh
+++ ci_scripts/release_charts.sh
@@ -16,56 +16,58 @@
 # specific language governing permissions and limitations
 # under the License.
 #
+echo $CI_COMMIT_MESSAGE
 
-pwd=$(pwd)
-cr="docker run -v ${pwd}/docs:/cr quay.io/helmpack/chart-releaser:v${CR_VERSION}"
-gitlab_repo="https://${GITLAB_USER}:${GITLAB_TOKEN}@${INTERNAL_GITLAB_URL}/devops-program/helm-charts"
-github_repo="helm-charts-test"
-helm_repo="https://helm.pingidentity.com/"
-chart="charts/ping-devops"
+# pwd=$(pwd)
+# cr="docker run -v ${pwd}/docs:/cr quay.io/helmpack/chart-releaser:v${CR_VERSION}"
+# gitlab_repo="https://${GITLAB_USER}:${GITLAB_TOKEN}@${INTERNAL_GITLAB_URL}/devops-program/helm-charts"
+# github_repo="helm-charts-test"
+# helm_repo="https://helm.pingidentity.com/"
+# chart="charts/ping-devops"
 
-git clone -b "${CI_COMMIT_BRANCH}" "${gitlab_repo}"
-cd helm-charts || exit
+# git clone -b "${CI_COMMIT_BRANCH}" "${gitlab_repo}"
+# cd helm-charts || exit
 
-function package_chart() {
-    echo "Packaging chart '${chart}'..."
-    helm package ${pwd}/${chart} --destination ${pwd}/docs/.chart-packages || exit
-}
+# function package_chart() {
+#     echo "Packaging chart '${chart}'..."
+#     helm package ${pwd}/${chart} --destination ${pwd}/docs/.chart-packages || exit 1
+# }
 
-function upload_packages() {
-    echo "Uploading chart packages for ${chart}..."
-    ${cr} upload -o ${GITHUB_OWNER} -r ${github_repo} --token ${GITHUB_TOKEN} --package-path /cr/.chart-packages || exit
-}
+# function upload_packages() {
+#     echo "Uploading chart packages for ${chart}..."
+#     ${cr} upload -o ${GITHUB_OWNER} -r ${github_repo} --token ${GITHUB_TOKEN} --package-path /cr/.chart-packages || exit 1
+# }
 
-function update_chart_index() {
-    echo "Generating chart index for ${chart}..."
-    ${cr} index -o ${GITHUB_OWNER} -r ${github_repo} -c ${helm_repo} --token ${GITHUB_TOKEN} --index-path /cr/index.yaml --package-path /cr/.chart-packages || exit
-    git status
-}
+# function update_chart_index() {
+#     echo "Generating chart index for ${chart}..."
+#     ${cr} index -o ${GITHUB_OWNER} -r ${github_repo} -c ${helm_repo} --token ${GITHUB_TOKEN} --index-path /cr/index.yaml --package-path /cr/.chart-packages || exit 1
+#     git status
+# }
 
-function publish_charts() {
-    branch=$(echo "$CI_COMMIT_MESSAGE" | awk '{print $2}')
-    (echo "$branch" | grep -Eq ^\d.\d.\d$) && echo "Release version passed formatting check, proceeding to GitHub push..." || echo "Release version DID NOT pass format check..." && exit
-    git config user.email "wesleymccollam@pingidentity.com"
-    git config user.name "wesleymccollam"
-    #change this to the real repo
-    git clone "https://wesleymccollam:${GITHUB_TOKEN}@github.com/wesleymccollam/helm-charts-test.git"
-    git remote add gh_location "https://wesleymccollam:${GITHUB_TOKEN}@github.com/wesleymccollam/helm-charts-test.git"
-    cd helm-charts-test
-    git add docs/index.yaml
-    git checkout -b ${branch} || exit
-    yes | cp ${pwd}/docs/index.yaml docs/index.yaml
-    git commit -m="Release ${branch}" --signoff
-    if test -n "$CI_COMMIT_TAG"; then
-        git push gh_location "$CI_COMMIT_TAG"
-    fi
-    git push gh_location origin master
-}
+# function publish_charts() {
+#     echo "$CI_COMMIT_MESSAGE"
+#     branch=$(echo "$CI_COMMIT_MESSAGE" | awk '{print $2}')
+#     (echo "$branch" | grep -Eq ^\d.\d.\d$) && echo "Release version passed formatting check, proceeding to GitHub push..." || echo "Release version DID NOT pass format check..." && exit 1
+#     git config user.email "wesleymccollam@pingidentity.com"
+#     git config user.name "wesleymccollam"
+#     #change this to the real repo
+#     git clone "https://wesleymccollam:${GITHUB_TOKEN}@github.com/wesleymccollam/helm-charts-test.git"
+#     git remote add gh_location "https://wesleymccollam:${GITHUB_TOKEN}@github.com/wesleymccollam/helm-charts-test.git"
+#     cd helm-charts-test
+#     git add docs/index.yaml
+#     git checkout -b ${branch} || exit
+#     yes | cp ${pwd}/docs/index.yaml docs/index.yaml
+#     git commit -m="Release ${branch}" --signoff
+#     if test -n "$CI_COMMIT_TAG"; then
+#         git push gh_location "$CI_COMMIT_TAG"
+#     fi
+#     git push gh_location origin master
+# }
 
-# install cr
-docker pull quay.io/helmpack/chart-releaser:v${CR_VERSION}
+# # install cr
+# docker pull quay.io/helmpack/chart-releaser:v${CR_VERSION}
 
-package_chart ${chart}
-upload_packages
-update_chart_index
-publish_charts
+# package_chart ${chart}
+# upload_packages
+# update_chart_index
+# publish_charts
