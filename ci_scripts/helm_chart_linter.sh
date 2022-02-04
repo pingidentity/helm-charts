bd4c06ccdaf12ef0a732e99f598af535b2333023
devops_program <wesleymccollam@pingidentity.com>
2022-02-04 12:25:04 -0600

    shfmt helm linter script

--- ci_scripts/helm_chart_linter.sh
+++ ci_scripts/helm_chart_linter.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if test -n "$CI_COMMIT_BRANCH"; then
-  git clone -b "$CI_COMMIT_BRANCH" "https://${GITLAB_USER}:${GITLAB_TOKEN}@${INTERNAL_GITLAB_URL}/devops-program/helm-charts"
+    git clone -b "$CI_COMMIT_BRANCH" "https://${GITLAB_USER}:${GITLAB_TOKEN}@${INTERNAL_GITLAB_URL}/devops-program/helm-charts"
 else
-  git clone "https://${GITLAB_USER}:${GITLAB_TOKEN}@${INTERNAL_GITLAB_URL}/devops-program/helm-charts"
+    git clone "https://${GITLAB_USER}:${GITLAB_TOKEN}@${INTERNAL_GITLAB_URL}/devops-program/helm-charts"
 fi
 
 cd helm-charts || exit
