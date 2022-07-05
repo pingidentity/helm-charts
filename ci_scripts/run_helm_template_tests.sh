0062c0e39cab8cb346b36941ddf2bfaf2893d04d
Henry Recker <henryrecker@pingidentity.com>
2022-07-05 19:57:24 +0000

    Add template yaml testing to pipeline
    Added simpler tests of helm template output to the pipeline.
    See README.md in helm-tests/template-tests for details.
    
    BRASS-447

---
+++ ci_scripts/run_helm_template_tests.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Ping Identity DevOps - CI scripts
+#
+# Run yaml unit tests with helm template.
+#
+# NOTE: To run individual tests locally, you can run the test_helm_template.py script directly.
+#
+
+# Run each test file found in the template-tests directory
+find ./helm-tests/template-tests -type f -print0 | while IFS= read -r -d '' _file; do
+    _testFilename=$(basename "${_file}")
+    _extension=${_testFilename##*.}
+    if test "${_testFilename}" != ".gitlab-ci.yml" && (test "${_extension}" = "yaml" || test "${_extension}" = "yml"); then
+        ./ci_scripts/test_helm_template.py test --test-file "${_file}"
+    fi
+done
